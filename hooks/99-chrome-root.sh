ff69092f0d0d0b5b4bcc3edf79a7e53e40856a07
j5pux <63794670+j5pux@users.noreply.github.com>
2022-01-28 02:40:41 +0100

    auto commit

--- hooks/99-chrome-root.sh
+++ hooks/99-chrome-root.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-find='"$HERE/chrome"'
-line='"$HERE/chrome" --user-data-dir --test-type --no-sandbox'
-for i in google-chrome google-chrome-beta; do
-    file="/bin/${i}"
-    grep -q -- "${line}" "${file}" || sed -i "s|${find}|${line}|" "${file}"
-done
+replace() {
+    line="${1} --user-data-dir --test-type --no-sandbox"
+    grep "${line}" "${2}"
+    grep -q "${line}" "${2}" || sed -i "s|${1}|${line}|" "${2}"
+}
+#find='"$HERE/chrome"'
+#line='"$HERE/chrome" --user-data-dir --test-type --no-sandbox'
+#file='/opt/google/chrome-beta/google-chrome-beta'
+#grep -q -- "${line}" "${file}" || sed -i "s|${find}|${line}|" "${file}"
 
-find='$CHROMIUM_FLAGS'
-line='$CHROMIUM_FLAGS --user-data-dir --test-type --no-sandbox'
-file="/bin/chromium"
-grep -q -- "${line}" "${file}" || sed -i "s|${find}|${line}|g" "${file}"
+#find='$CHROMIUM_FLAGS'
+#line='$CHROMIUM_FLAGS --user-data-dir --test-type --no-sandbox'
+#file='/bin/chromium'
+# grep -q -- "${line}" "${file}" || sed -i "s|${find}|${line}|g" "${file}"
+
+replace '"$HERE/chrome"' /opt/google/chrome-beta/google-chrome-beta
+replace '$LIBDIR/$APPNAME $CHROMIUM_FLAGS' /bin/chromium
