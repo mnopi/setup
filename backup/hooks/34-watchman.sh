220480e654b0b8813d78dd1f271cbb0a19604765
j5pux <63794670+j5pux@users.noreply.github.com>
2022-01-29 07:57:53 +0100

    auto commit

---
+++ backup/hooks/34-watchman.sh
@@ -0,0 +1,2 @@
+https://github.com/facebook/watchman/commit/2985377eaf8c8538b28fae9add061b67991a87c2
+https://unix.stackexchange.com/questions/438064/failed-to-determine-supplementary-groups-operation-not-permitted
