3bfe09ceed2f796e5d223aeb4bc66e33d41cbd61
j5pux <63794670+j5pux@users.noreply.github.com>
2022-01-29 05:46:03 +0100

    auto commit

--- watchman.sh
+++ watchman.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-watchman -j <<-EOT
-["trigger", "$(dirname "$0")", {
-  "name": "setup",
-  "command": ["git", "all"]
-}]
-EOT
+# watchman -j <<-EOT
+# ["trigger", "$(dirname "$0")", {
+#   "name": "setup",
+#   "command": ["git", "all"]
+# }]
+# EOT
+
+
+cat "$(watchman get-sockname | jq -r .sockname)"/log
+watchman --logfile=/tmp/setup.log --log-level 2
+watchman watch-del /root/setup
+watchman watch-project .
+watchman -- trigger . 'setup' -- git all
+watchman watch-list
+watchman trigger-list .
