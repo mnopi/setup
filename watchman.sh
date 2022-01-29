8257816d7d358f4609ffef283386f6dab2b085c8
j5pux <63794670+j5pux@users.noreply.github.com>
2022-01-29 07:46:51 +0100

    auto commit

--- watchman.sh
+++ watchman.sh
@@ -5,13 +5,14 @@ watchman get-config "${directory}"
 # watchman --logfile=/tmp/setup.log --log-level 2
 watchman watch-del-all
 watchman watch-del "${directory}"
+watchman -- trigger "${directory}" 'setup' -- git all
 
-watchman  -j <<-EOT
-["trigger", "${directory}", {
-  "command": ["git", "all"],
-  "name": "setup"
-}]
-EOT
+# watchman  -j <<-EOT
+# ["trigger", "${directory}", {
+#   "command": ["git", "all"],
+#   "name": "setup"
+# }]
+# EOT
 
 watchman watch-list
 watchman trigger-list "${directory}"
